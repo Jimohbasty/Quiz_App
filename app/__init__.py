5f52a08fd5a6605a4091f59a6a0609095b0f1c6d
dimka90 <dimkayilrit@gmail.com>
2024-05-23 17:22:42 +0100

    created the user table and database

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+# creating database
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+
+    db.init_app(app)
+    with app.app_context():
+        from . import models
+   
+    return app
